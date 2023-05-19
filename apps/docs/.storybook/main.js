b075567f604d33f84b6cbb879e34fa0639c694c4
yxf <1848212420@qq.com>
2023-05-19 21:00:00 +0800

    chore(file): rename

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -14,10 +14,10 @@ module.exports = {
       resolve: {
         alias: [
           {
-            find: "@acme/core",
+            find: "@soul/core",
             replacement: path.resolve(
               __dirname,
-              "../../../packages/acme-core/"
+              "../../../packages/soul-core/"
             ),
           },
         ],
